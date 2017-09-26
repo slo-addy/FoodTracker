39139d994c96b088283868d815a2b136df1c6059
Addison Francisco <addison.francisco@Addisons-MacBook-Pro.local>
2017-09-26 00:57:52 -0700

    Created new UITableViewController for meals list and added some sample data.

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Addison Francisco on 9/26/17.
+//  Copyright © 2017 Addison Francisco. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+	
+	// MARK: Properties
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var photoImageView: UIImageView!
+	@IBOutlet weak var ratingControl: RatingControl!
+	
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,86 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Addison Francisco on 9/26/17.
+//  Copyright © 2017 Addison Francisco. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+	
+	// MARK: Properties
+	
+	var meals = [Meal]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Load sample data
+		loadSampleMeals()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		// Table view cells are reused and should be dequeued using a cell identifier.
+		let cellIdentifier = "MealTableViewCell"
+		
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+			fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+		}
+		
+		let meal = meals[indexPath.row]
+		
+		cell.nameLabel.text = meal.name
+		cell.photoImageView.image = meal.photo
+		cell.ratingControl.rating = meal.rating
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
+	// MARK: Private Methods
+	private func loadSampleMeals() {
+		
+		let photo1 = UIImage(named: "meal1")
+		let photo2 = UIImage(named: "meal2")
+		let photo3 = UIImage(named: "meal3")
+		
+		guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+			fatalError("Unable to instantiate meal1")
+		}
+		
+		guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+			fatalError("Unable to instantiate meal2")
+		}
+		
+		guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
+			fatalError("Unable to instantiate meal3")
+		}
+		
+		meals += [meal1, meal2, meal3]
+	}
+}
+
+
+
+
+
